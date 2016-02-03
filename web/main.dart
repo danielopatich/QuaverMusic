e41757474cd192f03e39ab054d1694413c694380
Daniel Opatich <daniel.opatich@gmail.com>
2016-02-03 15:24:46 -0600

    added moving elements and clearing elements

--- web/main.dart
+++ web/main.dart
@@ -3,6 +3,45 @@
 
 import 'dart:html';
 
+List<ButtonElement> buttons = new List();
+Element doughBowl;
+Element grid;
+ButtonElement resetButton;
+
+
 void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
+  doughBowl = querySelector("#doughBowl");
+  grid = querySelector('#grid');
+
+  resetButton = querySelector("#resetButton");
+  resetButton.onClick.listen(reset);
+
+  produceDough();
 }
+
+void drag(Event e) {
+  Element dough = e.target;
+
+  if (dough.parent == doughBowl) {
+    grid.children.add(dough);
+  } else {
+    doughBowl.children.add(dough);
+  }
+}
+
+void reset (Event e) {
+  print('You reset the dough!');
+  grid.children.clear();
+  produceDough();
+}
+
+produceDough() {
+  buttons.clear();
+  for (var i = 0; i < 8; i++) {
+    buttons.add(new ButtonElement());
+    buttons[i].classes.add("dough");
+    buttons[i].onClick.listen(drag);
+    doughBowl.children.add(buttons[i]);
+  }
+}
+
