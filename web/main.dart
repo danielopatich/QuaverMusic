7d6c03d5679dacefd3c785d20e4011401e7fe2da
Daniel Opatich <daniel.opatich@gmail.com>
2016-02-04 15:19:06 -0600

    needs StageXL

--- web/main.dart
+++ web/main.dart
@@ -2,21 +2,67 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'dart:html';
+//import 'package:stagexl/stagexl.dart';
 
 List<ButtonElement> buttons = new List();
+CanvasElement dough = querySelector('dough');
 Element doughBowl;
 Element grid;
 ButtonElement resetButton;
 
+//Stage stage = new Stage(querySelector('#stage'));
+//
+//RenderLoop renderLoop = new RenderLoop();
+
+//class Dough extends Sprite {
+//  Dough(int width, int height) {
+//    graphics.rect(0, 0, width, height);
+//
+//    // need resource manager ...? //
+//    Dough<BitmapData> dough = BitmapData.load('./assets/dough-beat-1.png');
+//    dough.then((BitmapData bitmapData) {
+//
+//      Bitmap bitmap = new Bitmap(bitmapData);
+//      bitmap.scaleX = 0.5;
+//      bitmap.scaleY = 0.5;
+//      bitmap.x = 50;
+//      bitmap.y = 30;
+//      stage.addChild(bitmap);
+//    });
+//  }
+//}
 
 void main() {
   doughBowl = querySelector("#doughBowl");
   grid = querySelector('#grid');
-
   resetButton = querySelector("#resetButton");
   resetButton.onClick.listen(reset);
-
   produceDough();
+
+//  var dough = new Dough(25, 25);
+//  dough.x = 50;
+//  dough.y = 50;
+//  dough.addTo(stage);
+//
+//  bool lockCenter = true;
+//
+//  dough.onMouseDown.listen((me) => dough.startDrag(lockCenter));
+//  dough.onMouseUp.listen((me) => dough.stopDrag());
+}
+
+void reset (Event e) {
+  print('You reset the dough!');
+  grid.children.clear();
+  produceDough();
+}
+
+produceDough() {
+  for (var i = 0; i < 8; i++) {
+    buttons.add(new ButtonElement());
+    buttons[i].classes.add("dough");
+    buttons[i].onClick.listen(drag);
+    doughBowl.children.add(buttons[i]);
+  }
 }
 
 void drag(Event e) {
@@ -28,20 +74,3 @@ void drag(Event e) {
     doughBowl.children.add(dough);
   }
 }
-
-void reset (Event e) {
-  print('You reset the dough!');
-  grid.children.clear();
-  produceDough();
-}
-
-produceDough() {
-  buttons.clear();
-  for (var i = 0; i < 8; i++) {
-    buttons.add(new ButtonElement());
-    buttons[i].classes.add("dough");
-    buttons[i].onClick.listen(drag);
-    doughBowl.children.add(buttons[i]);
-  }
-}
-
